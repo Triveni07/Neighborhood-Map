4a462742b26232a44c3e179d5cfaa0abc4a8d5b2
LAPTOP-RTVOEFJC\vikra <triveni.wadkar@gmail.com>
2018-08-18 02:23:22 +0530

    Add basic map view to the web page

--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
 
 class Map extends Component {
+
     render() {
+        // load the googleMapUrl and api key withScriptJS HOC in react-google-maps 
+        const NeighborhoodMap = withScriptjs(withGoogleMap(props =>
+            <GoogleMap
+                defaultZoom={10}
+                defaultCenter={{ lat: 59.3293, lng: 18.0686 }}>
+            </GoogleMap>
+        ));
+
         return (
-            <div></div>
+            <div>
+                <NeighborhoodMap
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?libraries=places,geometry,drawing&key=AIzaSyCW83YC217OdmY33WAmZqeFGr9DiCfbKAw&v=3&callback=initMap"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `400px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                />
+
+            </div>
+
         );
+
     }
 };
 export default Map;
