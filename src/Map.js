3e68f190b428a20973d202e0ccad1dd643432971
LAPTOP-RTVOEFJC\vikra <triveni.wadkar@gmail.com>
2018-08-18 02:27:36 +0530

    Add location marker to the web page

--- src/Map.js
+++ src/Map.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 class Map extends Component {
 
+
     render() {
+
         // load the googleMapUrl and api key withScriptJS HOC in react-google-maps 
         const NeighborhoodMap = withScriptjs(withGoogleMap(props =>
             <GoogleMap
                 defaultZoom={10}
                 defaultCenter={{ lat: 59.3293, lng: 18.0686 }}>
+                <Marker
+                    position={{ lat: 59.3293, lng: 18.0686 }} />
             </GoogleMap>
         ));
 
@@ -19,7 +23,7 @@ class Map extends Component {
                     loadingElement={<div style={{ height: `100%` }} />}
                     containerElement={<div style={{ height: `400px` }} />}
                     mapElement={<div style={{ height: `100%` }} />}
-                />
+                    isMarkerShown />
 
             </div>
 
